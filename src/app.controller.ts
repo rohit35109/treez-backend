98791c97ae302735201ed8ad0ca21f46e407580d
Rohit Kumar <rohitkumar@Rohits-MacBook-Pro.local>
2023-04-05 21:35:02 +0530

    completed with the api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,15 +2,15 @@ import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 import { TransactionPayload } from './interface/transaction.interface';
 
-@Controller("transaction")
+@Controller('transaction')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getTransactions(
-    @Query("search") search: string = "",
+    @Query('search') search: string = '',
     // Use comma (,) to filter based on multiple status. eg: initiated, authorized
-    @Query("status") status: string = ""
+    @Query('status') status: string = '',
   ): TransactionPayload[] {
     return this.appService.getTransactions(search, status);
   }
--- src/app.service.ts
+++ src/app.service.ts
@@ -4,21 +4,28 @@ import { TransactionPayload } from './interface/transaction.interface';
 
 @Injectable()
 export class AppService {
-  
-  getTransactions(searchedText: string = "", status: string = ""): TransactionPayload[] {
-
-    const statusFilter = status.split(",");
+  getTransactions(
+    searchedText: string = '',
+    status: string = '',
+  ): TransactionPayload[] {
+    const statusFilter = status.split(',').map(i => i.toLowerCase());
     let records = this.getRandomData();
 
-    if (searchedText !== "") {
-      records = records.filter((item) => item.customer.includes(searchedText) || item.external_id.includes(searchedText) ||
-            item.gross_amount.toString().includes(searchedText) || item.source.includes(searchedText) ||
-            item.swifter_id.includes(searchedText));
+    // if searched text has values/not empty then it will execute this condition to filter data based on the searched text.
+    if (searchedText !== '') {
+      records = records.filter(
+        (item) =>
+          item.customer.includes(searchedText) ||
+          item.external_id.includes(searchedText) ||
+          item.gross_amount.toString().includes(searchedText) ||
+          item.source.includes(searchedText) ||
+          item.swifter_id.includes(searchedText),
+      );
     }
 
-    console.log(statusFilter.includes('initiated'));
-    if (statusFilter && statusFilter.length > 0) {
-      records = records.filter((item) => statusFilter.includes(item.status));
+    // if status has values then it will execute this condition to filter data based on the status
+    if (status !== "" && statusFilter && statusFilter.length > 0) {
+      records = records.filter((item) => statusFilter.includes(item.status.toLowerCase()));
     }
 
     return records;
@@ -26,12 +33,18 @@ export class AppService {
 
   /**
    * This funtion can be replaced by Database queries or typeorm.
-   * Currently this functions generates 50 random data that will be used to populate in the UI 
+   * Currently this functions generates 50 random data that will be used to populate in the UI
    * @returns TransactionPayload[]
    */
   getRandomData(): TransactionPayload[] {
     const data: TransactionPayload[] = [];
-    const customerNames = ['Eren Akichi', 'Mikasa Ackerman', 'Armin Arlert', 'Levi Ackerman', 'Sasha Braus'];
+    const customerNames = [
+      'Eren Akichi',
+      'Mikasa Ackerman',
+      'Armin Arlert',
+      'Levi Ackerman',
+      'Sasha Braus',
+    ];
     const sources = ['Payments', 'E-commerce', 'In-Store'];
     const fixedDate = '2022-12-10T02:10:00+00:00';
 
@@ -48,13 +61,12 @@ export class AppService {
         customer: customerNames[Math.floor(i % 5)],
         swifter_id: `S123IU${i}`,
         external_id: `TA0Z123${i}`,
-        source: sources[Math.floor(i % 3)]
-      }
+        source: sources[Math.floor(i % 3)],
+      };
 
       data.push(randomData);
     }
 
     return data;
   }
-
 }
